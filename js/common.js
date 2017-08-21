702cb47696aad43c0d2a9e68b5bae08e9f515154
Bogdan Peregon <peregonbw@gmail.com>
2017-08-21 12:05:49 +0300

    v2
    21.08

--- js/common.js
+++ js/common.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
         }, 800);
         return false;
     });
+
     $(function() {
         var shrinkHeader = 1;
         $(window).scroll(function() {
@@ -16,7 +17,15 @@ $(document).ready(function() {
                 $('.candibober-brand').css("display", "none");
                 $('.header-lang-block-selected').css("border-bottom", "4px solid white");
                 $('.hamburger .line').css("background-color", "white");
-                $('nav.header-menu').css("top", "60px");
+                $('.header-menu').css("top", "60px");
+
+                $(".header-menu>li").hover(function() {
+                    $(this).css("border-bottom", "4px solid white");
+                }, function() {
+                    $(this).css("border-bottom", "4px solid transparent");
+                });
+
+                
             } else {
                 $('header').removeClass('shrink');
                 $('li').removeClass('white');
@@ -24,27 +33,76 @@ $(document).ready(function() {
                 $('.candibober-brand').css("display", "inherit");
                 $('.header-lang-block-selected').css("border-bottom", "none");
                 $('.hamburger .line').css("background-color", "black");
-                $('nav.header-menu').css("top", "75px");
+                $('.header-menu').css("top", "75px");
+
+                $(".header-menu>li").hover(function() {
+                    $(this).css("border-bottom", "4px solid black");
+                }, function() {
+                    $(this).css("border-bottom", "4px solid transparent");
+                });
+
             }
         });
+
         function getCurrentScroll() {
             return window.pageYOffset || document.documentElement.scrollTop;
         }
     });
+
+
+    $(window).ready(function() {
+        var winwidth = $(window).outerWidth();
+        if (winwidth > 991) {
+            $('.header-menu').css("display", "inherit");
+            $(".header-menu>li").hover(function() {
+                $(this).css("border-bottom", "4px solid black");
+            }, function() {
+                $(this).css("border-bottom", "4px solid transparent");
+            });
+
+        } else if (winwidth < 992) {
+            $(".header-menu").hide();
+            $(".header-menu>li").hover(function() {
+                $(this).css("border-left", "4px solid white");
+                $(this).css("border-bottom", "4px solid transparent");
+            }, function() {
+                $(this).css("border-left", "4px solid transparent");
+                $(this).css("border-bottom", "4px solid transparent");
+            });
+        }
+    });
+
+
     $(".hamburger").click(function() {
         $(this).toggleClass("is-active");
     });
     $('.hamburger').click(function() {
-    $('nav').slideToggle({ top: 'toggle' });
-});
+        $('.header-menu').slideToggle({ top: 'toggle' });
+
+    });
 
 });
 
 $(window).resize(function() {
     var winwidth = $(window).outerWidth();
-    if (winwidth > 991 - scrollbarWidth()) {
+    if (winwidth > 991) {
+        $('.header-menu').css("display", "flex");
+        $(".header-menu>li").hover(function() {
+            $(this).css("border-bottom", "4px solid black");
+        }, function() {
+            $(this).css("border-bottom", "4px solid transparent");
+        });
+        $('.hamburger').removeClass('is-active');
 
-    } else if (winwidth < 992 - scrollbarWidth()) {
-
+    } else if (winwidth < 992) {
+        $('.header-menu').css("display", "none");
+        $(".header-menu>li").hover(function() {
+                $(this).css("border-left", "4px solid white");
+                $(this).css("border-bottom", "4px solid transparent");
+        }, function() {
+                $(this).css("border-left", "4px solid transparent");
+                $(this).css("border-bottom", "4px solid transparent");
+        });
+        $('.hamburger').removeClass('is-active');
     }
 });
