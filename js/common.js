a043b3144661206e6e87d407bf969e84be96eff6
Bogdan Peregon <peregonbw@gmail.com>
2017-08-21 10:17:17 +0300

    v2
    21.08

--- js/common.js
+++ js/common.js
@@ -15,17 +15,36 @@ $(document).ready(function() {
                 $('.brand-text').addClass('brand-text-e');
                 $('.candibober-brand').css("display", "none");
                 $('.header-lang-block-selected').css("border-bottom", "4px solid white");
+                $('.hamburger .line').css("background-color", "white");
+                $('nav.header-menu').css("top", "60px");
             } else {
                 $('header').removeClass('shrink');
                 $('li').removeClass('white');
                 $('.brand-text').removeClass('brand-text-e');
                 $('.candibober-brand').css("display", "inherit");
                 $('.header-lang-block-selected').css("border-bottom", "none");
+                $('.hamburger .line').css("background-color", "black");
+                $('nav.header-menu').css("top", "75px");
             }
         });
-
         function getCurrentScroll() {
             return window.pageYOffset || document.documentElement.scrollTop;
         }
     });
+    $(".hamburger").click(function() {
+        $(this).toggleClass("is-active");
+    });
+    $('.hamburger').click(function() {
+    $('nav').slideToggle({ top: 'toggle' });
+});
+
+});
+
+$(window).resize(function() {
+    var winwidth = $(window).outerWidth();
+    if (winwidth > 991 - scrollbarWidth()) {
+
+    } else if (winwidth < 992 - scrollbarWidth()) {
+
+    }
 });
