63c711f77dcf58c69a2fc947420bb60e74933222
Bogdan Peregon <peregonbw@gmail.com>
2017-09-21 14:02:13 +0300

    12

--- contacts.php
+++ contacts.php
@@ -1,16 +1,21 @@
-<?
-if (array_key_exists('messageFF', $_POST)) {
-   $to = 'peregonb@i.ua';
-   $subject = 'Заполнена контактная форма с '.$_SERVER['HTTP_REFERER'];
-   $subject = "=?utf-8?b?". base64_encode($subject) ."?=";
-   $message = 
-   "Имя: ".$_POST['nameFF'].
-   "\nТелефон: ".$_POST['contactFF'].
-   "\nIP: ".$_SERVER['REMOTE_ADDR'];
-   $headers = 'Content-type: text/plain; charset="utf-8"';
-   $headers .= "MIME-Version: 1.0\r\n";
-   $headers .= "Date: ". date('D, d M Y h:i:s O') ."\r\n";
-   mail($to, $subject, $message, $headers);
-   echo $_POST['nameFF'];
+ <?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'peregonb@i.ua'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Обратный звонок'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                    <h3>Запрос на обратный звонок. </h3>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['phone'].'</p>                        
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Заполнена контактная форма <gardini@mail.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+        echo "Dgsdg";
 }
 ?>
--- js/common.js
+++ js/common.js
@@ -1,36 +1,42 @@
 $(document).ready(function() {
-$('.slider2').find('.slick-list').find('.slick-track').children('.slick-active').eq(1).addClass('center');
+    // $('.slider2').find('.slick-list').find('.slick-track').children('.slick-active').eq(1).addClass('center');
     $('.services-carousel').slick({
         arrows: true,
+        infinite: false,
         dots: true
-    }); 
-       $('.slider2').slick({
+    });
+
+    $('.slider2').slick({
         arrows: true,
         dots: false,
+        lazyLoad: 'ondemand',
         infinite: true,
-  slidesToShow: 3,
-  centerMode: true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        centerMode: false
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1
-      }
-    }
-  ]
+        slidesToShow: 3,
+        centerMode: true,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    centerMode: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+    $('.slider2').slickLightbox({
+        itemSelector: 'a',
+        navigateByKeyboard: true
     });
     $("#top").click(function() {
         $("body, html").animate({
@@ -38,37 +44,45 @@ $('.slider2').find('.slick-list').find('.slick-track').children('.slick-active')
         }, 800);
         return false;
     });
-
-$('.index-bg').css("height", "100vh");
-$('.index-content').css("height", "100vh");
-$('.index-button').css("margin-top", "10vh");
-$('.down-button').css("bottom", "3vh");
-
-function scrollbarWidth() {
-    var block = $('<div>').css({ 'height': '50px', 'width': '50px' }),
-        indicator = $('<div>').css({ 'height': '200px' });
-
-    $('body').append(block.append(indicator));
-    var w1 = $('div', block).innerWidth();
-    block.css('overflow-y', 'scroll');
-    var w2 = $('div', block).innerWidth();
-    $(block).remove();
-    return (w1 - w2);
-}
-
-
-$(window).resize(function() {
     var winwidth = $(window).outerWidth();
-    if (winwidth > 991 - scrollbarWidth()) {
-        $('.header-menu').css("display", "flex");
-        $('.header-menu').css("display", "-webkit-flex");
-        $('.hamburger').removeClass('is-active');
+    if (winwidth > 322 - scrollbarWidth()) {
+        $('.index-bg').css("height", "100vh");
+        $('.index-content').css("height", "100vh");
 
-    } else if (winwidth < 992 - scrollbarWidth()) {
-        $('.header-menu').css("display", "none");
-        $('.hamburger').removeClass('is-active');
+    } else if (winwidth < 321 - scrollbarWidth()) {
+        $('.index-bg').css("height", "120vh");
+        $('.index-content').css("height", "120vh");
     }
-});
+
+
+    $('.index-button').css("margin-top", "10vh");
+    $('.down-button').css("bottom", "3vh");
+
+    function scrollbarWidth() {
+        var block = $('<div>').css({ 'height': '50px', 'width': '50px' }),
+            indicator = $('<div>').css({ 'height': '200px' });
+
+        $('body').append(block.append(indicator));
+        var w1 = $('div', block).innerWidth();
+        block.css('overflow-y', 'scroll');
+        var w2 = $('div', block).innerWidth();
+        $(block).remove();
+        return (w1 - w2);
+    }
+
+
+    $(window).resize(function() {
+        var winwidth = $(window).outerWidth();
+        if (winwidth > 991 - scrollbarWidth()) {
+            $('.header-menu').css("display", "flex");
+            $('.header-menu').css("display", "-webkit-flex");
+            $('.hamburger').removeClass('is-active');
+
+        } else if (winwidth < 992 - scrollbarWidth()) {
+            $('.header-menu').css("display", "none");
+            $('.hamburger').removeClass('is-active');
+        }
+    });
 
 
     $(function() {
@@ -124,59 +138,43 @@ $(window).resize(function() {
     });
     $('.hamburger').click(function() {
         $('.header-menu').slideToggle({ top: 'toggle' });
-        
+
     });
 
 });
 
 function initMap() {
-        var uluru = { lat: 50.402335, lng: 30.624691 };
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 17,
-            center: uluru
-        });
-        var marker = new google.maps.Marker({
-            position: uluru,
-            map: map,
-            icon: {
-                url: "img/mark.svg",
-                scaledSize: new google.maps.Size(29, 35)
-            }
-        });
-    }
+    var uluru = { lat: 50.402335, lng: 30.624691 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        gestureHandling: 'greedy',
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+        icon: {
+            url: "img/mark.svg",
+            scaledSize: new google.maps.Size(29, 35)
+        }
+    });
+}
 
 $('a[href^="#"]').click(function() {
-$('html,body').animate({ scrollTop: $(this.hash).offset().top-60});
-return false;
-e.preventDefault();
+    $('html,body').animate({ scrollTop: $(this.hash).offset().top - 60 });
+    return false;
+    e.preventDefault();
 });
 
 
-    $('.header-lang-block-1').on('click', function(e) {
-      $('.header-lang-block-1').addClass("header-lang-block-selected"); //you can list several class names 
-      $('.header-lang-block-2').removeClass("header-lang-block-selected");
-    });
+$('.header-lang-block-1').on('click', function(e) {
+    $('.header-lang-block-1').addClass("header-lang-block-selected"); //you can list several class names 
+    $('.header-lang-block-2').removeClass("header-lang-block-selected");
+});
 
-    $('.header-lang-block-2').on('click', function(e) {
-      $('.header-lang-block-2').addClass("header-lang-block-selected"); //you can list several class names
-      $('.header-lang-block-1').removeClass("header-lang-block-selected"); 
-    });
+$('.header-lang-block-2').on('click', function(e) {
+    $('.header-lang-block-2').addClass("header-lang-block-selected"); //you can list several class names
+    $('.header-lang-block-1').removeClass("header-lang-block-selected");
+});
 
 
-document.getElementById('feedback-form').addEventListener('submit', function(evt){
-  var http = new XMLHttpRequest(), f = this;
-  evt.preventDefault();
-  http.open("POST", "contacts.php", true);
-  http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  http.send("nameFF=" + f.nameFF.value + "&contactFF=" + f.contactFF.value);
-  http.onreadystatechange = function() {
-    if (http.readyState == 4 && http.status == 200) {
-      alert(http.responseText + ', Ваше сообщение получено.\nМы Вам перезвоним в течении N часов.\nБлагодарю за сотрудничество!');    
-      f.messageFF.removeAttribute('value'); // очистить поле сообщения (две строки)
-      f.messageFF.value='';
-    }
-  }
-  http.onerror = function() {
-    alert('Ваше сообщение не получено. \nПопробуйте позже.');
-  }
-}, false);
